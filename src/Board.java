2c1fd1f89e4209a1bc6292ba0112bd5ba911a53c
t00201634 <Bryan.OShea@students.ittralee.ie>
2021-11-16 15:39:59 +0000

    attempt 2: still bad

--- src/Board.java
+++ src/Board.java
@@ -4,8 +4,6 @@ import java.awt.*;
 public class Board extends JPanel{
     private static final int BOARD_WIDTH = 512;
     private static final int BOARD_HEIGHT = 512;
-    private final int DIMENSION = 8;
-    private final int SQ_SIZE = 512 / DIMENSION;
 
     private final Color BLACK = Color.BLACK;
     // It is named WHITE for convention, the actual color will usually be different.
@@ -13,21 +11,27 @@ public class Board extends JPanel{
 
     // ref: https://github.com/tobyjamesthomas/Java-Checkers/blob/master/src/Checkers/Board.java
     public void paint(Graphics g){
-        g.fillRect(0,0,BOARD_WIDTH,BOARD_HEIGHT);
+        int DIMENSION = 8;
+        int SQ_SIZE = 512 / DIMENSION;
         for (int row = 0; row < DIMENSION; row++) {
             for (int column = 0; column < DIMENSION; column++) {
-                if (row % 2 == column % 2)
+                if (row % 2 == column % 2) {
                     g.setColor(BLACK);
-                else
+                } else {
                     g.setColor(WHITE);
-                g.fillRect(column*SQ_SIZE, row*SQ_SIZE, SQ_SIZE, SQ_SIZE);
+                }
+                g.fillRect(column * SQ_SIZE, row * SQ_SIZE, SQ_SIZE, SQ_SIZE);
             }
+            System.out.println(row * SQ_SIZE);
         }
     }
 
+    // ref: https://www.youtube.com/watch?v=no4m-TIX-rc
+    // ref: https://1bestcsharp.blogspot.com/2015/04/Java-How-To-Draw-Chess-Board-In-Java-Swing-Using-NetBeans.html
     public static void main(String[] args) {
         JFrame frame = new JFrame();
-        frame.setSize(527,600);
+        // board isn't aligned properly, gets clipped off.
+        frame.setSize(528,551);
         frame.getContentPane().add(new Board());
         frame.setBackground(Color.GRAY);
         frame.setLocationRelativeTo(null);
