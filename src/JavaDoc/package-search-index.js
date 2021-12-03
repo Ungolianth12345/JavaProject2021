3081d57f259db4ff3415b4237be611d32a1cddce
t00201634 <Bryan.OShea@students.ittralee.ie>
2021-12-03 12:37:38 +0000

    Final commit

--- src/JavaDoc/package-search-index.js
+++ src/JavaDoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"Checkers"},{"l":"Pharmacy"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Checkers"},{"l":"Pharmacy"}];updateSearchResults();
